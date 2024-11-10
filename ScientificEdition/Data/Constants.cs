49986232e1afc7b6e3e6c0d804dd9b3e28b2c378
Dmitroson <sanyokkotenko2002@gmail.com>
2024-11-10 20:31:20 +0200

    Add user roles

---
+++ ScientificEdition/Data/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ScientificEdition.Data
+{
+    public class UserRoles
+    {
+        public const string Author = "Author";
+        public const string Admin = "Admin";
+        public const string Reviewer = "Reviewer";
+    }
+}
---
+++ ScientificEdition/Data/Migrations/20241110182451_SeedRoles.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScientificEdition.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [AspNetRoles] ([Id], [Name], [NormalizedName]) VALUES (NEWID(), 'Author', 'AUTHOR')");
+            migrationBuilder.Sql("INSERT INTO [AspNetRoles] ([Id], [Name], [NormalizedName]) VALUES (NEWID(), 'Admin', 'ADMIN')");
+            migrationBuilder.Sql("INSERT INTO [AspNetRoles] ([Id], [Name], [NormalizedName]) VALUES (NEWID(), 'Reviewer', 'REVIEWER')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [AspNetRoles] WHERE [Name] IN ('Author', 'Admin', 'Reviewer')");
+        }
+    }
+}
