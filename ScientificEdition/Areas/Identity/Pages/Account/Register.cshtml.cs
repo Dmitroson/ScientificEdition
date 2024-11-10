b2313441b3282fe429f9c85bf13f17fccffcf7d4
Dmitroson <sanyokkotenko2002@gmail.com>
2024-11-10 21:35:42 +0200

    Update User-related views

--- ScientificEdition/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ScientificEdition/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,23 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
 using ScientificEdition.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace ScientificEdition.Areas.Identity.Pages.Account
 {
@@ -46,57 +39,42 @@ namespace ScientificEdition.Areas.Identity.Pages.Account
             _emailSender = emailSender;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string ReturnUrl { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required]
+            [MinLength(2, ErrorMessage = "Мінімальна довжина: {1} символи.")]
+            [MaxLength(50, ErrorMessage = "Максимальна довжина: {1} символів.")]
+            [Display(Name = "Ім'я")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [MinLength(2, ErrorMessage = "Мінімальна довжина: {1} символи.")]
+            [MaxLength(50, ErrorMessage = "Максимальна довжина: {1} символів.")]
+            [Display(Name = "Прізвище")]
+            public string LastName { get; set; }
+
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "E-mail")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [MinLength(6, ErrorMessage = "Мінімальна довжина паролю {0} символів.")]
+            [MaxLength(100, ErrorMessage = "Максимальна довжина паролю {0} символів.")]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Пароль")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Підтвердити пароль")]
+            [Compare("Password", ErrorMessage = "Паролі не співпадають.")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -114,6 +92,7 @@ namespace ScientificEdition.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                InitializeUser(user, Input);
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
@@ -129,7 +108,7 @@ namespace ScientificEdition.Areas.Identity.Pages.Account
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        values: new { area = "Identity", userId, code, returnUrl },
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
@@ -137,7 +116,7 @@ namespace ScientificEdition.Areas.Identity.Pages.Account
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl });
                     }
                     else
                     {
@@ -167,6 +146,12 @@ namespace ScientificEdition.Areas.Identity.Pages.Account
                     $"Ensure that '{nameof(User)}' is not an abstract class and has a parameterless constructor, or alternatively " +
                     $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
+        }
+
+        private static void InitializeUser(User user, InputModel model)
+        {
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
         }
 
         private IUserEmailStore<User> GetEmailStore()
--- ScientificEdition/Data/Entities/User.cs
+++ ScientificEdition/Data/Entities/User.cs
@@ -7,5 +7,7 @@ namespace ScientificEdition.Data.Entities
         public required string FirstName { get; set; }
 
         public required string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
