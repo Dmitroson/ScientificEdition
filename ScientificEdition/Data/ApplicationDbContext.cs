ba3230018ffda1b6368b6eedbfd357b6db666c44
Dmitroson <sanyokkotenko2002@gmail.com>
2024-11-10 20:18:47 +0200

    Add project files.

---
+++ ScientificEdition/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ScientificEdition.Data.Entities;
+
+namespace ScientificEdition.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .Property(x => x.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Entity<User>()
+                .Property(x => x.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
