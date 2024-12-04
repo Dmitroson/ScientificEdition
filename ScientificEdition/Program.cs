f00e61f71bb61ec05a836724ff9d8f7b33baa8b4
Dmitroson <sanyokkotenko2002@gmail.com>
2024-12-04 10:46:14 +0200

    Enable automigrations

--- ScientificEdition/Program.cs
+++ ScientificEdition/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 ConfigureServices(builder.Services);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -24,15 +23,17 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
